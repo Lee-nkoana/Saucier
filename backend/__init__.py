e7f0f6f1770e481e068d12c3ad5825a3f53f4c9c
Lebohang Nkoana <lenkoanjhb024@student.wethinkcode.co.za>
2025-09-28 21:57:59 +0200

    added login page

--- backend/__init__.py
+++ backend/__init__.py
@@ -18,5 +18,12 @@ def create_app():
     @app.route("/")
     def home():
         return render_template("index.html")
+    
+    @app.route("/login")
+    def login():
+        return render_template("login.html")
 
     return app
+
+
+

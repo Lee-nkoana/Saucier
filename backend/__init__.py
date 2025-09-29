f758e6fdc3f5fac1df5193d3de5288144e36ed28
Lebohang Nkoana <lenkoanjhb024@student.wethinkcode.co.za>
2025-09-29 12:41:36 +0200

    update login and register styling

--- backend/__init__.py
+++ backend/__init__.py
@@ -22,6 +22,10 @@ def create_app():
     @app.route("/login")
     def login():
         return render_template("login.html")
+    
+    @app.route("/register")
+    def register():
+        return render_template("register.html")
 
     return app
 
